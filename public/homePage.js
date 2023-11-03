9e18d3cde7eeee823e7331f99ee8a73a57319341
TimurVetrov <timurworkvetrov@gamil.com>
2023-11-03 13:14:52 +0300

    Graduate work

---
+++ public/homePage.js
@@ -0,0 +1,102 @@
+'use strict';
+
+const logoutButton = new LogoutButton();
+
+logoutButton.action = () => ApiConnector.logout(
+	(response) => {
+		if (response.success) {
+			location.reload();
+		}
+	}
+);
+
+ApiConnector.current((response) => {
+	if (response.success) {
+		ProfileWidget.showProfile(response.data);
+	}
+});
+
+const ratesBoard = new RatesBoard();
+
+function stocksTimer() {
+	ApiConnector.getStocks((response) => {
+		if (response.success) {
+			ratesBoard.clearTable();
+			ratesBoard.fillTable(response.data);
+		}
+	});
+}
+stocksTimer();
+
+let timerId = setInterval(() => stocksTimer(), 50000);
+
+const moneyManager = new MoneyManager();
+
+moneyManager.addMoneyCallback = (data) =>
+	ApiConnector.addMoney(data, (response) => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(response.success, 'Операция прошла успешно!')
+		} else {
+			moneyManager.setMessage(response.success, response.error);
+		}
+	}
+	);
+
+moneyManager.conversionMoneyCallback = (data) =>
+	ApiConnector.convertMoney(data, (response) => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(response.success, 'Операция прошла успешно!')
+		} else {
+			moneyManager.setMessage(response.success, response.error);
+		}
+	}
+    );
+
+moneyManager.sendMoneyCallback = (data) =>
+	ApiConnector.transferMoney(data, (response) => {
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+			moneyManager.setMessage(response.success, 'Операция прошла успешно!')
+		} else {
+			moneyManager.setMessage(response.success, response.error);
+		}
+	}
+	);
+
+const favoritesWidget = new FavoritesWidget();
+
+ApiConnector.getFavorites((response) => {
+    if (response.success) {
+        favoritesWidget.clearTable();
+        favoritesWidget.fillTable(response.data);
+        moneyManager.updateUsersList(response.data);
+    }
+});
+    
+favoritesWidget.addUserCallback = (data) =>
+    ApiConnector.addUserToFavorites(data, (response) => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoritesWidget.setMessage(response.success, 'Операция выполнена успешно!')
+        } else {
+            favoritesWidget.setMessage(response.success, response.error);
+        }
+    }
+    );
+    
+favoritesWidget.removeUserCallback = (data) =>
+    ApiConnector.removeUserFromFavorites(data, (response) => {
+        if (response.success) {
+            favoritesWidget.clearTable();
+            favoritesWidget.fillTable(response.data);
+            moneyManager.updateUsersList(response.data);
+            favoritesWidget.setMessage(response.success, 'Операция выполнена успешно!')
+        } else {
+            favoritesWidget.setMessage(response.success, response.error);
+        }
+    }
+    );
